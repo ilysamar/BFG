7ce00c8af26ddfdd7114b0928ab562bf33fe1aef
Ilya Samartsev <ilysamar@gmail.com>
2022-06-08 10:37:25 +0300

    v0.3

--- app/app.py
+++ app/app.py
@@ -5,9 +5,8 @@ import time
 from flask import Flask, render_template, flash, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
 load_dotenv(find_dotenv())
-#
+
 app = Flask(__name__)
 connectionString = 'postgresql+psycopg2://{***REMOVED***}:{passwd}@{host}:{port}/{db}'.format(
     ***REMOVED***=os.environ.get('POSTGRES_USER'),
@@ -16,14 +15,6 @@ connectionString = 'postgresql+psycopg2://{***REMOVED***}:{passwd}@{host}:{port}
     port=os.environ.get('POSTGRES_PORT'),
     db=os.environ.get('POSTGRES_DB'))
 
-# connectionString = 'postgresql+psycopg2://{***REMOVED***}:{passwd}@{host}:{port}/{db}'.format(
-#     ***REMOVED***=os.environ.get('POSTGRES_USER', '***REMOVED***'),
-#     passwd=os.environ.get('POSTGRES_PASSWORD', '***REMOVED***'),
-#     host=os.environ.get('POSTGRES_HOST', 'test_app_db_1'),
-#     port=os.environ.get('POSTGRES_PORT', '5432'),
-#     db=os.environ.get('POSTGRES_DB', 'testdb'))
-# print(connectionString)
-# connectionString = "postgresql://***REMOVED***:***REMOVED***@test_app_db_1/testdb"
 app.config['SQLALCHEMY_DATABASE_URI'] = connectionString
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -32,7 +23,6 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 
 db = SQLAlchemy(app)
-# db.create_all()
 
 class Cand(db.Model):
     id = db.Column('cand_id', db.Integer, primary_key=True)
@@ -46,16 +36,6 @@ class Cand(db.Model):
         self.about = about
 
 
-def database_initialization_sequence():
-    db.create_all()
-    test_rec = Cand(
-            'Илья',
-            'Самарцев',
-            'Программист')
-
-    db.session.add(test_rec)
-    db.session.rollback()
-    db.session.commit()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -76,7 +56,6 @@ def home():
 
 
 if __name__ == '__main__':
-    print("Start:" + "01")
     dbstatus = False
     while dbstatus == False:
         try:
@@ -86,5 +65,4 @@ if __name__ == '__main__':
             print("ERROR:" + "DB")
         else:
             dbstatus = True
-    database_initialization_sequence()
     app.run(debug=True, host='0.0.0.0', port=8080)
