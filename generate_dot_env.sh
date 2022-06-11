42a63abff79d94b2fe2f0360f44e01121537efac
Ilya Samartsev <ilysamar@gmail.com>
2022-06-11 10:26:34 +0300

    delete .env and add bash script

---
+++ generate_dot_env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "-------------------"
+echo "Generate file .env"
+echo "-------------------"
+
+# Generate symbol string
+for symbol in {A..Z} {a..z} {0..9}; do
+  SYMBOLS=$SYMBOLS$symbol
+done
+SYMBOLS=$SYMBOLS'!@#$%&*()?/\[]{}-+_=<>.,'
+
+
+# Generate ***REMOVED***name
+for (( i=1; i <= 8; i++ )); do
+***REMOVED***=$***REMOVED***${SYMBOLS:$(expr $RANDOM % ${#SYMBOLS}):1}
+done
+
+
+# Generate password
+for (( i=1; i <= 16; i++ )); do
+pass=$pass${SYMBOLS:$(expr $RANDOM % ${#SYMBOLS}):1}
+done
+
+
+echo "POSTGRES_USER = \"***REMOVED***_$***REMOVED***\"" | tee .env
+echo "POSTGRES_PASSWORD = \"$pass\"" | tee -a .env
+echo "POSTGRES_HOST = \"test_app_db_1\"" | tee -a .env
+echo "POSTGRES_PORT = \"5432\"" | tee -a .env
+echo "POSTGRES_DB = \"testdb\"" | tee -a .env
+
+mkdir "folder"
+cp .env app/
+
